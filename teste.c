7f4ecb4fa61acc227e7013eff516e68304b5e37c
DaviParada <daviparada532@gmail.com>
2024-09-17 20:26:47 -0300

    new

---
+++ teste.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h> // Inclui a biblioteca stdbool.h para obter os limites dos tipos primitivos
+int main (){
+    bool condicao = true;  // Declara uma variável booleana 'condicao' e a inicializa com o valor true (verdadeiro)
+    if (condicao) // Verifica se a condição é verdadeira
+    printf ("A condição é verdadeira\n"); // Se for verdadeira, imprime "A condição é verdadeira"
+
+}
+
