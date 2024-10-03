17230b56b4648a27512ca28b5b9ec948c3fea718
DaviParada <daviparada532@gmail.com>
2024-10-03 17:02:13 -0300

    new

---
+++ Aula-8.c
@@ -0,0 +1,49 @@
+/* --- Nível 1 --- */
+
+/* Exercício 1*/
+int main(){
+int matriz[3][3];
+for (int i = 0; i < 3; i++){
+    for (int j = 0; j < 3; j++){
+        printf("Digite um numero:\n");
+        scanf("%d", &matriz[i][j]);
+    }
+}
+
+for (int i = 0; i < 3; i++){
+    for (int j = 0; j < 3; j++){
+        printf("%d ",matriz[i][j]);
+    }
+    printf("\n");
+}
+return 0;
+}
+
+/* Exercício 2 */
+int main (){
+int matriz[3][3];
+for (int i = 0; i < 3; i++){
+    for (int j = 0; j < 3; j++){
+        printf("Digite um numero:\n");
+        scanf("%d", &matriz[i][j]);
+    }
+}
+
+int main (){
+int matriz[3][3], soma = 0;
+for (int i = 0; i < 3; i++){
+    for (int j = 0; j < 3; j++){
+        printf("Digite um numero:\n");
+        scanf("%d", &matriz[i][j]);
+    }
+}
+for (int i = 0; i < 3; i++){
+    for (int j = 0; j < 3; j++){
+        
+    }
+}
+
+
+return 0;
+}
+
