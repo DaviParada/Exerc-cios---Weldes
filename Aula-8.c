7c841be45f5bd7046527b761a4e780c6eb01c780
DaviParada <daviparada532@gmail.com>
2024-10-04 10:21:51 -0300

    New
    Fiz até o exercício 1 do nível 2

--- Aula-8.c
+++ Aula-8.c
@@ -101,6 +101,7 @@ return 0;
 
 
 /* Exercício 5 */
+/*
 int main (){
 int matriz[3][3], tmatriz[3][3];
 
@@ -122,3 +123,40 @@ for (int i = 0; i < 3; i++){
 
 return 0; 
 }
+*/
+
+
+/* --- Nível 2 --- */
+
+/* Exercício 1 */
+int main (){
+int matriz[3][4], Soma[3] = {0, 0, 0};
+for (int i = 0; i < 3; i++){
+    for (int j = 0; j < 4; j++){
+        printf("Digite um numero:\n");
+        scanf("%d", &matriz[i][j]);
+        Soma[i] += matriz[i][j];
+    }
+}
+
+printf("\n-- MATRIZ RESULTANTE --\n");
+
+for (int i = 0; i < 3; i++){
+    for (int j = 0; j < 4; j++){
+        matriz[i][j] *= Soma[i];
+        printf("%d ", matriz[i][j]);
+    }
+    printf("\n");
+}
+
+return 0;
+}
+
+
+
+
+
+
+
+
+
