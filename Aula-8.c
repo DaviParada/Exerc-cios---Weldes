6cbfb2b6670d154794ff834206fc8a50c16c3ad9
DaviParada <daviparada532@gmail.com>
2024-10-04 09:35:41 -0300

    new

--- Aula-8.c
+++ Aula-8.c
@@ -74,9 +74,51 @@ return 0;
 
 
 /* Exercício 4 */
+/*
 int main (){
+int matriz[3][3];
 
+for (int i = 0; i < 3; i++){
+    for (int j = 0; j < 3; j++){
+        printf("Digite um numero:\n");
+        scanf("%d", &matriz[i][j]);
+    }
+}
 
+for (int i = 0; i < 3; i++){
+    for(int j = 0; j < 3; j++){
+        if (matriz[i][j] != matriz [j][i]){
+            printf("A matriz nao eh simetrica!");
+            return 0;
+        }
+    }
+}
+printf("A matriz eh simetrica!");
 
 return 0;
+}
+*/
+
+
+/* Exercício 5 */
+int main (){
+int matriz[3][3], tmatriz[3][3];
+
+for (int i = 0; i < 3; i++){
+    for (int j = 0; j < 3; j++){
+        printf("Digite um numero:\n");
+        scanf("%d", &matriz[i][j]);
+        tmatriz[i][j] = matriz[j][i];
+    }
+}
+
+for (int i = 0; i < 3; i++){
+    for (int j = 0; j < 3; j++){
+        tmatriz[i][j] = matriz[j][i];
+        printf("%d ", tmatriz[i][j]);   
+    }
+    printf("\n");
+}
+
+return 0; 
 }
