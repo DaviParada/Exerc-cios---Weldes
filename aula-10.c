09c4dc862018b3121134dc0b3d7da1bbe5e480b1
DaviParada <daviparada532@gmail.com>
2024-10-18 09:20:11 -0300

    new

--- aula-10.c
+++ aula-10.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
 int soma(int m, int n){
     return m + n;
 }
@@ -26,3 +27,62 @@ int main(){
     printf("m * n = %d\n", multiplicacao(m, n));
     printf("m / n = %d\n", divisao(m, n));
 }
+*/
+
+/* Exercício - 1 */
+/*
+int quadrado(int num){
+    return num * num;
+}
+int main(){
+    int num;
+    printf("Insira um numero para saber o seu quadrado: \n");
+    scanf("%d", &num);
+    printf("%d", quadrado(num));
+}
+*/
+
+/* Exercício - 2 */
+/*
+int ParouImpar(num){
+    if (num % 2 == 0) return 1;
+    return 0;
+    }
+int main (){
+    int num;
+    printf("Digite um numero: \n");
+    scanf("%d", &num);
+    if(ParouImpar(num)){
+        printf("Esse numero eh par!");
+    } else if(num == 0){
+        printf("Esse numero eh neutro!");
+    } else {
+        printf("Esse numero eh impar!");
+    }
+
+}
+*/
+
+/* Exercício - 3 */
+/*
+float media(float num1, float num2, float num3){
+    return printf("A media eh: %f", ((num1 + num2 + num3) / 3));
+}
+int main(){
+    float num1, num2, num3;
+    printf("Digite o primeiro numero: \n");
+    scanf("%f", &num1);
+    printf("Digite o segundo numero: \n");
+    scanf("%f", &num2);
+    printf("Digite o terceiro numero: \n");
+    scanf("%f", &num3);
+    media(num1, num2, num3);
+
+    return 0;
+}
+*/
+
+/* Exercício - 4 */
+
+
+
