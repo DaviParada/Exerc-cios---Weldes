ea699f78501a418833311cd7c590b1aace96827b
Davi Parada <daviparada532@gmail.com>
2024-11-07 10:11:21 -0300

    Exercício para aula 11
    Criar uma pasta para Aula 11 e colocar esse exercício

--- aula-10.c
+++ aula-10.c
@@ -148,3 +148,37 @@ int main(){
 
 
 
+
+/* AULA 11 */
+#include <stdio.h>
+#include <stdlib.h>
+
+void incrementa(int *valor){
+    printf("Antes de icrementar\n");
+    printf("O contador vale %d\n", (*valor));
+    printf("O endereço de memória eh %d\n", valor);
+
+    printf("Depois de incrementar\n");
+    printf("O contador vale %d\n", ++(*valor));
+    printf("O endereço de memória eh %d\n", valor);
+}
+
+int main () {
+    int cont = 10;
+
+    printf("Antes de icrementar\n");
+    printf("O Contador vale %d\n", cont);
+    printf("O endereço de memória eh %d\n", &cont);
+
+    incrementa(&cont);
+
+    printf("Depois de icrementar\n");
+    printf("O contador vale %d\n", cont);
+    printf("O endereço de memória eh %d\n", &cont);
+
+    return 0;
+}
+
+
+
+
